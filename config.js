5c5289936a0885c71ed6161c2c2781285181f037
kihangim <kihangim@gmail.com>
2019-12-04 18:09:09 +0900

    initialize blog setting

---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  url: 'https://yeri-kim.github.io/',
+  title: 'Blog by Yeri',
+  subtitle: 'hello world',
+  copyright: '',
+  disqusShortname: 'github-io-12',
+  postsPerPage: 6,
+  googleAnalyticsId: 'UA-139726433-1',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    }
+    /*
+    ,
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+    */
+  ],
+  author: {
+    name: 'Yeri',
+    photo: '/profile.png',
+    bio: '',
+    contacts: {
+      email: 'yerikim1028@gmail.com',
+      github: 'Yeri-Kim',
+      linkedIn: 'yeri-kim-1028'
+    }
+  }
+};
